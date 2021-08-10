5a8b6926e0914ecfc25dbb0f9ba8837ef0f59bd9
scailerprz <88690808+scailerprz@users.noreply.github.com>
2021-08-10 12:06:44 -0500

    Add files via upload

---
+++ src/desmenu.js
@@ -0,0 +1,41 @@
+const desmenu = (prefix, pushname) => {
+    return `
+*Comandos De Descargas ✅*
+ 
+══════════════
+
+*AUDIO*
+
+${prefix}play2
+Una alternativa por si *play llego a su limite de canciones
+
+${prefix}play
+Coloca el nombre de la cancion, o el link del video YT
+
+*Ejemplo*
+
+${prefix}play https://www.youtube.com/xxxxxxxxxxxxx
+
+══════════════
+
+*EN MANTENIMIENTO 🛠*
+
+~${prefix}ytmp3~ EN MANTENIMIENTO
+
+~${prefix}ytmp4~ EN MANTENIMIENTO
+
+ESTAS FUNCIONES ESTAN EN MANTENIMIENTO
+ESTATE ATENTO A LA ACTUALIZACION QUE LANZARE PRONTO
+
+Coloca *version
+Para conocer tu version y checar la actualizacion mas reciente de tu bot
+
+══════════════
+    
+_*play y *play2 tiene un limite de 2000 canciones en el server, si llega a su limite se restablecera dentro de 24 horas_
+     
+ву ѕнαη∂υу`
+
+}
+
+exports.desmenu = desmenu
---
+++ src/version.js
@@ -0,0 +1,27 @@
+const version = (prefix, pushname) => {
+    return `
+*ѕнαηвσт ву ѕнαη∂υу*
+
+*Actualizado:* 25 de julio de 2021
+*Versión actual:* 3.2
+*Ofrecida por:* ѕнαη∂υу™
+
+
+*INFORME*
+
+Si no funciona el comando *play o *play2 checa el blog del bot y mira la version que poses 
+Ya que ire cambiando mi apikey de mi bot para que siga en funcionamiento
+Cambiare la apikey pasando un mes o si veo muchos atercados o robos
+Si vez a alguien que robo mi bot comunicate conmigo con el comando *creador
+
+
+*BLOG DE SHANBOT*
+
+https://cutt.ly/jmSF2RN
+
+ву ѕнαη∂υу
+`
+
+}
+
+exports.version = version
